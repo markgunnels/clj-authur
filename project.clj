4c95a1783fdf5b201c9e5d6d24887a7e9cb209bf
Mark Gunnels <markgunnels@gmail.com>
2012-02-09 04:47:49 -0500

    updated clj-http

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-authur "0.0.1-SNAPSHOT"
   :description "Client for authur.wilbur.io"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.2.3"]
+                 [clj-http "0.3.1"]
                  [org.clojure/data.json "0.1.1"]]
   :dev-dependencies [[swank-clojure "1.3.4-SNAPSHOT"]])
